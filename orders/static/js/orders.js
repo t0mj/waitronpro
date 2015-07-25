b167dc540658a9c0cf740e60df0bda1fd9ba2e71
Thomas Johns <stratic@gmail.com>
2015-07-25 18:36:24 -0500

    Implement sub options

--- orders/static/js/orders.js
+++ orders/static/js/orders.js
@@ -1,19 +1,20 @@
 wp.ordersView = (function() {
     var self = {};
 
-    self.match = ko.observable();
     self.products = ko.observableArray();
     self.productsByType = ko.observableArray();
     self.productTypes = ko.observableArray();
     self.options = ko.observableArray();
-    self.optionsByProduct = ko.observableArray();
     self.tagOptions = ko.observableArray();
+    self.productTagOptions = ko.observableArray();
+    self.subOptionTags = ko.observableArray();
 
     self.retrieveActiveOptions = function() {
         $.getJSON("api/retrieveActiveOptions", function(data) {
             $.each(data, function(key, val) {
                 self.options.push(new wp.models.Option().mapDict(val));
             });
+            self.retrieveActiveProducts();
         });
     };
 
@@ -22,6 +23,7 @@ wp.ordersView = (function() {
             $.each(data, function(key, val) {
                 self.products.push(new wp.models.Product().mapDict(val));
             });
+            self.retrieveSubOptions();
         });
     };
 
@@ -30,6 +32,7 @@ wp.ordersView = (function() {
             $.each(data, function(key, val) {
                 self.productTypes.push(new wp.models.Type().mapDict(val));
             });
+            viewModel.retrieveActiveOptions();
         });
     };
 
@@ -45,47 +48,65 @@ wp.ordersView = (function() {
     self.tempArray = ko.observableArray([]);
 
     self.getOptions = function(productModel) {
-        self.tagOptions.removeAll();
-        // self.optionsByProduct.removeAll();
-        if (productModel.subOptions()) {
-            var subOptionTags = productModel.subOptions().split(",");
-            for (var i=0; i < subOptionTags.length; i++) {
-                var model = new wp.models.OptionByTag();
-                model.tag = subOptionTags[i];
-                var options = self.options();
-                for (var n=0; n < self.options().length; n++) {
-                    if (options[n].tag() === subOptionTags[i]) {
-                        debugger;
-                        self.tempArray.push(options[n]);
-                        // model.options.push(option);
-                    }
-                }
-                model.optionsArray = self.tempArray();
-                self.tagOptions.push(model);
+        // Get all options based on the product chosen
+        var tagOptions = self.tagOptions();
+        var subOptions = $.map(productModel.subOptions().split(","), $.trim);
+        for (var i=0; i < tagOptions.length; i++) {
+            var optionTag = tagOptions[i].tag();
+            if (subOptions.indexOf(optionTag) !== -1) {
+                self.productTagOptions.push(tagOptions[i]);
             }
         }
     };
 
-    // self.getOptions = function(productModel) {
-    //     // self.optionsByProduct.removeAll();
-    //     if (productModel.subOptions()) {
-    //         self.subOptionTags(productModel.subOptions().split(","));
-    //     }
-    // };
-    //
-    // self.getSubOptions = function(tag) {
-    //     console.log(tag);
-    //     ko.utils.arrayForEach(self.options(), function(option) {
-    //         if (tag === option.tag()) {
-    //             self.optionsByProduct.push(option);
-    //             // return option();
-    //         }
-    //     });
-    //     self.match = ko.utils.arrayGetDistinctValues(self.optionsByProduct())[0];
-    //     // debugger;
-    //     return self.match;
-    // };
-    //
+    self.retrieveSubOptions = function() {
+        var products = self.products();
+        var options = self.options();
+        var tags = self.subOptionTags();
+
+        // sort through products for suboption tags
+        for (var y=0; y < products.length; y++) {
+            if (products[y].subOptions()) {
+                tempTags = products[y].subOptions().split(",");
+                for (var tag of tempTags) {
+                    tag = $.trim(tag);
+                    if (tags.indexOf(tag) === -1) {
+                        tags.push(tag);
+                    }
+                }
+            }
+        }
+
+        /*
+        sort through suboptiontags for matching options
+        create model for tag and an array of the options
+        */
+        if (tags.length > 0) {
+            for (var i=0; i < tags.length; i++) {
+                var tempArray = [];
+                for (var n=0; n < options.length; n++) {
+                    if (options[n].tag() === tags[i]) {
+                        tempArray.push(options[n]);
+                    }
+                }
+                if (tempArray.length > 0) {
+                    var model = new wp.models.OptionByTag();
+                    model.tag(tags[i]);
+                    model.optionsArray(tempArray);
+                    self.tagOptions.push(model);
+                }
+            }
+        }
+    };
+
+    self.addItem = function(productModel) {
+        self.productTagOptions.removeAll();
+        if (productModel.subOptions()) {
+            self.getOptions(productModel);
+            $('#suboption-modal').modal('show');
+        }
+    };
+
     self.poop = function(newValue) {
         console.log(newValue);
         console.log('something');
@@ -96,5 +117,3 @@ wp.ordersView = (function() {
 var viewModel = new wp.ordersView;
 ko.applyBindings(viewModel, $('#page-content')[0]);
 viewModel.retrieveProductTypes();
-viewModel.retrieveActiveProducts();
-viewModel.retrieveActiveOptions();
